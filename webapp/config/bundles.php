de629b96ab09290e0ce1082be7c142862ba0726a
Keith Johnson <kj@ubergeek42.com>
2019-11-07 16:44:36 -0500

    Add sentry
    composer require sentry/sentry-symfony:^3.2

--- webapp/config/bundles.php
+++ webapp/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
